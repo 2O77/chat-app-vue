95f72752a9e4ad0c77c2845cfe3cf7dd68ceff23
2o77 <recepahmetkara@gmail.com>
2023-09-22 21:44:21 +0300

    something

---
+++ client/src/components/sidebar.js
@@ -0,0 +1,12 @@
+import React from "react"
+import '../styles/sidebar.css'
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            
+        </div>
+    )
+}
+
+export default Sidebar
