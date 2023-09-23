943809e3e9f6e81b4cd796b039fde762e7e18cf6
2o77 <recepahmetkara@gmail.com>
2023-09-23 23:59:12 +0300

    sidebar styling

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -1,10 +1,25 @@
 import React from "react"
 import '../styles/sidebar.css'
+import CreateChatIcon from '../images/icons8-new-message-90.png'
+import FriendsIcon from '../images/icons8-users-90.png'
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
-            
+            <div className="sidebar-header">
+                <div className="profile-section">
+                    <div className="profile-picture"></div>
+                </div>
+                <div className="icon-section">
+                    <div className="icon-container">
+                        <img src={FriendsIcon} alt='Create-Chat-Icon' className="header-icon"/>
+                    </div>
+                    <div className="icon-container">
+                        <img src={CreateChatIcon} alt='Create-Chat-Icon' className="header-icon"/>
+                    </div>
+                </div>
+            </div>
+            <div className="sidebar-context"></div>
         </div>
     )
 }
