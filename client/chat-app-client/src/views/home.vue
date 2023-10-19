b2cb28e0ef0face44ef0b04940961be6b79609b7
2o77 <recepahmetkara@gmail.com>
2023-10-19 16:56:35 +0300

    sj

---
+++ client/chat-app-client/src/views/home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>
+            <h1>abc</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home" 
+    }
+</script>
+
+<style scoped>
+
+</style>
