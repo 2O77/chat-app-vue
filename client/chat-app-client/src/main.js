212a31eeeb3037a458ff180f2e7996c76ea5fe54
2o77 <recepahmetkara@gmail.com>
2023-10-19 16:40:53 +0300

    router functionality occured

--- client/chat-app-client/src/main.js
+++ client/chat-app-client/src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from "axios"
 
 const app = createApp(App)
-
 app.use(router)
+app.config.globalProperties.$axios = axios;
+
 
 app.mount('#app')
--- client/chat-app-client/src/router/index.js
+++ client/chat-app-client/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from "@/components/Home"
-import Register from "@/components/register"
-import Login from "@/components/login"
+import Login from "../views/login.vue"
+import Register from "../views/register.vue"
+import Home from "../views/home.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
